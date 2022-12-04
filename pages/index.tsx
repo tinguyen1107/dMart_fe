ef07a3825f6940f0ee23455cc63d83cc00cedd1f
ToTuanAn <73640290+ToTuanAn@users.noreply.github.com>
2022-12-04 15:10:30 +0700

    header + footer base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
-import { Box, HStack, Text, VStack } from '@chakra-ui/react';
+import { Box, 
+  HStack, 
+  Text, } from '@chakra-ui/react';
 import Head from 'next/head';
 import { ReactElement } from 'react';
-import { LoginButton } from '../components';
+import { LoginButton, Footer, NavBar} from '../components';
 import { MainLayout } from '../layouts';
 import { NextPageWithLayout } from './_app';
+
 
 const HomePage: NextPageWithLayout = () => {
   return (
     <>
+    <NavBar />
       <Head>
         <title>Home page</title>
         <meta name="description" content="dMart" />
@@ -17,12 +21,18 @@ const HomePage: NextPageWithLayout = () => {
           <Text fontSize="24px" fontWeight="700">
             DMart
           </Text>
+          <Text fontSize="24px" fontWeight="700">
+            Feature
+          </Text>
           <LoginButton />
         </HStack>
         <Text fontSize="20px" fontWeight="600">
           List NFT
         </Text>
       </Box>
+
+      <Footer/>
+      
     </>
   );
 };
